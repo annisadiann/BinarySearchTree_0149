1fe27cf2eab29eb6d0d89d3f733847d2380dbb0c
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2024-06-14 10:07:00 +0700

    menetapkan node baru sebagai root jika pohon kosong

--- binarysearch/binarysearch.cpp
+++ binarysearch/binarysearch.cpp
@@ -35,5 +35,11 @@ public:
 
 		search(element, parent, currentNode);   // Locate the node which will be the parent of the new node
 
+		if (parent == nullptr) {  // If the parent is NULL (Tree is empty)
+			ROOT = newNode;  // mark the new node as the root
+			return;  // exit
+		}
+
 	}
 };
+
